5fd5702e4855b55b00aec0b364d2d3233de54b91
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-06 10:12:27 +0000

    post

---
+++ app/Micropost.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content'];
+
+    /**
+     * この投稿を所有するユーザー
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
