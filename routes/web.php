3f8d4fc0aa6cfc9743947d787ca5f787d0eabf72
masashi.ishii <ishii@mobilesupport.co.jp>
2022-07-07 07:01:42 +0000

    FIX follow unfollow router

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['prefix' => 'users/{id}'], function () {
-        Route::post('follow', 'UserFollowController@store')->name('users.follow');
-        Route::delete('unfollow', 'UserFollowController@destroy')->name('users.unfollow');
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
         Route::get('followwings', 'UsersController@followings')->name('users.followings');
         Route::get('followers', 'UsersController@followers')->name('users.followers');
     });
